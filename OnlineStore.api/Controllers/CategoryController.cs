8ad0e22f67205d092b11a453d1a288e8250edcb2
Mostafa-Babaei <83201012+Mostafa-Babaei@users.noreply.github.com>
2022-11-14 01:37:26 +0330

    add category api

---
+++ OnlineStore.api/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using infrastructure.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public List<MainMenu> Get()
+        {
+            return categoryService.GetAllCategory();
+        }
+    }
+}
